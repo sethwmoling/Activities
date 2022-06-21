c5ec7e445cc74eac387a6823f639ade2146e1a82
Seth Moling <sethwmoling@gmail.com>
2022-06-20 21:25:24 -0500

    activities

---
+++ week3/3.1.1-FunctionDeclaration.js
@@ -0,0 +1,16 @@
+const greet = function() {
+  console.log("Hello!");
+};
+
+const threeModTwo = function() {
+  console.log(3 % 2);
+};
+
+let age = 18;
+const checkDrivingAge = function() {
+  if (age >= 16) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+};
---
+++ week3/3.1.7-ArrowSyntax.js
@@ -0,0 +1,22 @@
+function myNumberFunction() {
+  return ((2 ** 2 + 3) % 4) * 14;
+}
+
+function greet(name) {
+  return `Hello, ${name}`;
+}
+
+const timeOfDayGreet = function(name, timeOfDay) {
+  return `Hello, ${name}, good ${timeOfDay}`;
+};
+
+function tripleAndHalf(num) {
+  let triple = num * 3;
+  return triple / 2;
+}
+
+function sumTwoNumbers(num1, num2) {
+  let sum = num1 + num2;
+  console.log(`The sum of your numbers is ${sum}.`);
+  return sum;
+}
